aad010ae9f64d3ca3d03b5e06c9858caf5d7aab7
Iam-Mahi <mahender.koyada@gmail.com>
2024-10-01 10:41:30 +0530

    Update main.tf
    updated

--- main.tf
+++ main.tf
@@ -12,8 +12,13 @@ provider "aws" {
   region = "us-east-1"
 }
 
-service "aws_ec2" "test" {
+resource "aws_instance" "test" {
 ami           = "ami-0e86e20dae9224db8"
 instance_type = "t2.micro"
 
 }
+
+#tags = {
+#Name = var.ec2_name
+#}
+
